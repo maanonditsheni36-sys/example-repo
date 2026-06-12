c21c411850cb6c2ac596a32f47a146af6628159e
maanonditsheni36-sys <maanonditsheni36@gmail.com>
2026-06-12 17:48:22 +0200

    added a new file

---
+++ conversion.py
@@ -0,0 +1,17 @@
+#Declaring the variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+#Converting the variables 
+num1_int = int(num1)
+num2_float = float(num2)
+num3_str = str(num3)
+string1_int = int(string1)
+
+# Outputing the converted variables
+print(num1_int)
+print(num2_float)
+print(num3_str)
+print(string1_int)
